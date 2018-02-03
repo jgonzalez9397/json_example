77ea3ca47b8d2b891ddbfb3637958927bf2bfef0
jgonzalez9397 <jonathangonzalez144@gmail.com>
2018-02-03 15:51:16 -0500

    adding in the jason data for accounts

---
+++ accounts.js
@@ -0,0 +1,17 @@
+[
+{"first": "jonathan",
+  "last": "gonz", 
+ "balance": 100
+}, 
+
+{"first": "rich",
+  "last": "anth", 
+ "balance": 120
+},
+
+{"first": "pelayo",
+  "last": "flores", 
+ "balance": 150
+}
+
+]
